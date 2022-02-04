f0bc19eba7edb4e349bc616ee92f701cb408f0dd
Juenma <98423244+Juenma@users.noreply.github.com>
2022-02-04 20:05:46 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php 
+  $destino= "juanmapinna@gmail.com";
+  $nombre = $_POST["nombre"];
+  $apellido = $_POST["apellido"];
+  $email = $_POST["email"];
+  $mensaje = $_POST["mensaje"];
+  $contenido = "Nombre: " . $nombre . "\nApellido" .$apellido . "\nEmail" . $email . "\nMensaje" .$mensaje;
+  mail($destino,"Contacto", $contenido);
+  header("Location:gracias.html")
+
+
+
+?>
